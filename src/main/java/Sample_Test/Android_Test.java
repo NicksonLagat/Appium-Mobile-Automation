be91357463cffe6c53316e8cd963c1a32b3ad6c5
NicksonLagat <73611868+NicksonLagat@users.noreply.github.com>
2021-03-02 14:29:04 +0300

    appium inspection

--- src/main/java/Sample_Test/Android_Test.java
+++ src/main/java/Sample_Test/Android_Test.java
@@ -3,7 +3,9 @@ package Sample_Test;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -22,11 +24,33 @@ public class Android_Test {
         cap.setCapability("deviceName","emulator-5554 ");
         //cap.setCapability("app","C:/DummyApk/ApiDemos-debug.apk");
         cap.setCapability("automationName","UiAutomator2");
-        cap.setCapability("appPackage","io.appium.android.apis");
-        cap.setCapability("appActivity",".ApiDemos");
+        cap.setCapability("appPackage","com.google.android.dialer");
+        cap.setCapability("appActivity",".DialtactsActivity");
+        cap.setCapability("avd","Nexus_5_API_28");
 
-        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
-        Thread.sleep(200);
+
+        //driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+        //Thread.sleep(200);
+
+        //url to access the device
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
+
+        //new android driver object
+        AppiumDriver driver = new AppiumDriver(url,cap);
+        String sessionId =  driver.getSessionId().toString();
+        Thread.sleep(300);
+        System.out.println(sessionId);
+
+
+        driver.findElement(By.id("com.google.android.dialer:id/contacts_tab")).click();
+        System.out.println("pass");
+        driver.findElement(By.id("com.google.android.dialer:id/empty_list_view_action")).click();
+        Thread.sleep(35000);
+        driver.findElement(By.xpath("//android.widget.EditText[@text ='First name']")).sendKeys("Nick");
+        Thread.sleep(3000);
+
+
+
 
 
 
